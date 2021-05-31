53d27d56c4cf43332b79357ec4575864940cc558
revinaya@gmail <revinaya@gmail.com>
2021-06-01 00:29:26 +0400

    add 2 functions

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 function getRandomValue(min, max) {
-    if (min < 0 ) {
-       throw new RangeError('Parametr `min` is out of range')
-    }
-    if (max < 0 ) {
-       throw new RangeError('Parametr `max` is out of range')
-    }
-	if (max < min) {
-	   min = max
-	}
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+   if (min < 0) {
+      throw new RangeError('Parametr `min` is out of range')
+   }
+   if (max < 0) {
+      throw new RangeError('Parametr `max` is out of range')
+   }
+   if (max < min) {
+      min = max
+   }
+   return Math.floor(Math.random() * (max - min)) + min;
+}
 
-  function checkStringLength(str, max_length) {
-    return str.length <= max_length;
-  }
+function checkStringLength(str, max_length) {
+   return str.length <= max_length;
+}
 
+getRandomValue(1, 10);
+checkStringLength('TYFGHUTDGkjhh', 7);
