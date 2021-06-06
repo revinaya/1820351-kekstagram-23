27e6d0d5708fcbffd139810ff6e5c551fde619cb
revinaya@gmail <revinaya@gmail.com>
2021-06-06 20:33:12 +0400

    add 4.9 homework

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function getRandomValue(min, max) {
   }
   if (max < 0) {
     throw new RangeError('Parametr `max` is out of range');
-  }
+  } 
   if (max < min) {
     min = max;
   }
@@ -15,5 +15,85 @@ function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
-getRandomValue(1, 10);
-checkStringLength('TYFGHUTDGkjhh', 7);
+//getRandomValue(1, 10);
+//checkStringLength('TYFGHUTDGkjhh', 7);
+
+const COMMENTS_COUNT = 6;
+const SIMILAR_PHOTO_DESCRIPTION_COUNT = 25;
+
+const generateIndexes = (count) => {
+  return [...Array(count).keys()]
+};
+
+const DESCRIPTION_INDEXES = generateIndexes(SIMILAR_PHOTO_DESCRIPTION_COUNT)
+
+const generateRandomUniqueArray = (index_array) => {
+  arr = index_array.map((a) => a + 1);
+  arr.sort((a, b) => getRandomValue(1, 100) - getRandomValue(1, 100))
+  return arr
+};
+
+const IDS = generateRandomUniqueArray(DESCRIPTION_INDEXES);
+const URLS = generateRandomUniqueArray(DESCRIPTION_INDEXES).map((i) => "photos/" + i + ".jpg");
+const COMMENT_IDS = generateRandomUniqueArray(generateIndexes(SIMILAR_PHOTO_DESCRIPTION_COUNT * COMMENTS_COUNT));
+
+const DESCRIPTIONS = [
+  'Описание1',
+  'Описание2',
+  'Описание3',
+  'Описание4',
+  'Описание5',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Владимир',
+  'Казимир',
+  'Сигизмунд',
+  'Демьян',
+  'Константин',
+  'Владислав',
+  'Ерофей',
+  'Фадей',
+];
+
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomValue(0, elements.length - 1)];
+};
+
+const createComment = (description_idx, comment_idx) => {
+  return {
+    id: COMMENT_IDS[(description_idx +1) * (comment_idx+1) - 1],
+    avatar: "img/avatar-"+getRandomValue(1, 6)+".svg",
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  };
+};
+
+
+const createPhotoDescription = (idx) => {
+  return {
+    id: IDS[idx],
+    url: URLS[idx],
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomValue(15, 200),
+    comments: generateIndexes(COMMENTS_COUNT).map((a) => createComment(idx, a)),
+  };
+};
+
+
+const similarPhotoDescription = DESCRIPTION_INDEXES.map(createPhotoDescription);
+
+console.log(similarPhotoDescription);
+
+console.log(similarPhotoDescription[0].comments);
+
